7244ef105018c9aafe91463911b8060e67c74d36
DrZoid <docteurzoidberg@users.noreply.github.com>
2020-09-28 13:26:09 +0200

    r2d2 lego boost custom component first prototype

---
+++ src/custom_r2d2/custom_r2d2.h
@@ -0,0 +1,55 @@
+#include "esphome.h"
+#include "BoostHub.h"
+#include "Lpf2HubConst.h"
+
+using namespace esphome;
+
+static const char *TAG = "r2d2";
+
+BoostHub myBoostHub;
+
+class R2D2 : public Component, CustomAPIDevice  {
+  private:
+
+  public:
+    R2D2() : Component() {
+    }
+    void on_hello_world() {
+
+      ESP_LOGD("r2d2", "Hello World!");
+
+      ESP_LOGD("r2d2", "IsConnecting: %d", myBoostHub.isConnecting());
+      ESP_LOGD("r2d2", "IsConnected: %d", myBoostHub.isConnected());
+
+      if(!myBoostHub.isConnected()) {
+        ESP_LOGD("r2d2", "Not connected");
+        myBoostHub.init(5);
+        ESP_LOGD("r2d2", "IsConnecting: %d", myBoostHub.isConnecting());
+        if(myBoostHub.isConnecting()) {
+          ESP_LOGD("r2d2", "Connect hub");
+          myBoostHub.connectHub();
+        }
+        ESP_LOGD("r2d2", "IsConnected: %d", myBoostHub.isConnected());
+      }
+
+      //wait to be connected?
+      if (myBoostHub.isConnected()) {
+        char hubName[] = "DrZoid";
+        myBoostHub.setHubName(hubName);
+        myBoostHub.setLedColor(LpfLedColor::BLUE);
+        myBoostHub.setMotorSpeed(BoostHub::Port::D  , 100);
+        delay(3000);
+        myBoostHub.setMotorSpeed(BoostHub::Port::D , 0);
+        //myBoostHub.shutDownHub();
+      } else {
+         ESP_LOGD("r2d2", "still not connected");
+      }
+    }
+    void setup() override {
+      ESP_LOGCONFIG(TAG, "Setting up ...");
+      register_service(&R2D2::on_hello_world, "hello_world");
+    }
+    void loop() override {
+      //if not connected, scan and try to connect
+    }
+};

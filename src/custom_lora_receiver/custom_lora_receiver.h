60a6ee5a805169726917186e59762eb7b7cc993e
DrZoid <docteurzoidberg@users.noreply.github.com>
2020-10-19 11:48:55 +0200

    lora receiver new sensors

--- src/custom_lora_receiver/custom_lora_receiver.h
+++ src/custom_lora_receiver/custom_lora_receiver.h
@@ -13,10 +13,9 @@
 #include "myoleddisplay.h"
 #include "structures.h"
 
-
 using namespace esphome;
 
-static const char *TAG = "sensor.lora_receiver";
+static const char *TAG = "[LORA]";
 static const char* host = "lora-station1";
 
 #define  PIN_SCK    5
@@ -30,8 +29,9 @@ static const char* host = "lora-station1";
 
 MyOledDisplay 	display(0x3c, 4, 15);
 
-uint16_t 		packetCounter=0;
+uint16_t packetCounter=0;
 
+//timers
 volatile uint32_t lastPacketReceived=0;
 volatile uint32_t lastScreenUpdate=0;
 
@@ -52,14 +52,31 @@ static DevicePTDataStruct pt_state = {
   .deviceLowBatt=false
 };
 
+class LoraPacketSensor : public Component {
+  public:
+    TextSensor *last_packet_received = new TextSensor();
+};
+
 class LoraReceiver : public Component {
   private:
+
+    char const hex[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',   'B','C','D','E','F'};
+
+    std::string byte_2_str(unsigned char* bytes, int size) {
+      std::string str;
+      for (int i = 0; i < size; ++i) {
+        const unsigned char ch = bytes[i];
+        str.append(&hex[(ch  & 0xF0) >> 4], 1);
+        str.append(&hex[ch & 0xF], 1);
+      }
+      return str;
+    }
 
     void parse_packet(dzb::Packet const& packet) {
 
       if (!packet.is_crc_valid()) {
         // TODO: handle error
-        Serial.println("[PACKET] CRC error");
+        ESP_LOGD(TAG, "[PACKET] CRC error");
         return;
       }
 
@@ -75,83 +92,83 @@ class LoraReceiver : public Component {
 
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get TIMESTAMP\n", devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get TIMESTAMP\n", devicestring);
         }
 
         if(reader.get_value(dzb::PacketType::PRESENCE, 0, state.devicePirState)) {
           device_J7_pir_state->publish_state(state.devicePirState);
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get PRESENCE\n", devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get PRESENCE\n", devicestring);
         }
 
         if(reader.get_value(dzb::PacketType::ALARM, 0, state.deviceAlarmActive)) {
           device_J7_alarm_isactive->publish_state(state.deviceAlarmActive);
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get ALARM\n", devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get ALARM\n", devicestring);
         }
 
         if(reader.get_value(dzb::PacketType::BATT_LOW, 0, state.deviceLowBatt)) {
           device_J7_batt_islow->publish_state(state.deviceLowBatt);
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get BATT_LOW\n" , devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get BATT_LOW\n" , devicestring);
         }
 
         if(reader.get_value(dzb::PacketType::BATT_PERCENT, 0, state.deviceBattPercent)) {
           device_J7_batt_percent->publish_state(state.deviceBattPercent);
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get BATT_PERCENT\n", devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get BATT_PERCENT\n", devicestring);
         }
 
         if(reader.get_value(dzb::PacketType::BATT_VOLTAGE, 0, state.deviceBattVoltage)) {
           device_J7_batt_voltage->publish_state(state.deviceBattVoltage);
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get BATT_VOLTAGE\n", devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get BATT_VOLTAGE\n", devicestring);
         }
 
-        Serial.printf("[PACKET][FROM:%s][DATA] Alarm=%d|LowBatt=%d|BattVoltage=%0.2f|BattPercent=%d|Presence=%d\n", devicestring, state.deviceAlarmActive, state.deviceLowBatt, state.deviceBattVoltage, state.deviceBattPercent, state.devicePirState);
+        ESP_LOGD(TAG, "[PACKET][FROM:%s][DATA] Alarm=%d|LowBatt=%d|BattVoltage=%0.2f|BattPercent=%d|Presence=%d\n", devicestring, state.deviceAlarmActive, state.deviceLowBatt, state.deviceBattVoltage, state.deviceBattPercent, state.devicePirState);
       }
       //ANOTHER DEVICE
       else if(packet.is_from_device("PT")) {
         //another device
-        Serial.printf("[PACKET][FROM:%s][DATA] data...\n", devicestring);
+        ESP_LOGD(TAG, "[PACKET][FROM:%s][DATA] data...\n", devicestring);
 
         if(reader.get_value(dzb::PacketType::BATT_LOW, 0, pt_state.deviceLowBatt)) {
           device_PT_batt_islow->publish_state(pt_state.deviceLowBatt);
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get BATT_LOW\n" , devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get BATT_LOW\n" , devicestring);
         }
 
         if(reader.get_value(dzb::PacketType::BATT_PERCENT, 0, pt_state.deviceBattPercent)) {
           device_PT_batt_percent->publish_state(pt_state.deviceBattPercent);
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get BATT_PERCENT\n", devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get BATT_PERCENT\n", devicestring);
         }
 
         if(reader.get_value(dzb::PacketType::BATT_VOLTAGE, 0, pt_state.deviceBattVoltage)) {
           device_PT_batt_voltage->publish_state(pt_state.deviceBattVoltage);
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get BATT_VOLTAGE\n", devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get BATT_VOLTAGE\n", devicestring);
         }
 
         if(reader.get_value(dzb::PacketType::DISTANCE, 0, pt_state.deviceWaterLevel)) {
           device_PT_water_level->publish_state(pt_state.deviceWaterLevel);
         }
         else {
-          Serial.printf("[PACKET][FROM:%s][ERROR] Could not get WATER LEVEL\n", devicestring);
+          ESP_LOGD(TAG, "[PACKET][FROM:%s][ERROR] Could not get WATER LEVEL\n", devicestring);
         }
 
       }
       //UNKNOWN DEVICE
       else {
-        Serial.printf("[PACKET][FROM:%s][ERROR] Unknown device %s\n", devicestring, devicestring);
+        ESP_LOGE(TAG, "[PACKET][FROM:%s][ERROR] Unknown device %s\n", devicestring, devicestring);
       }
 
     }
@@ -161,32 +178,42 @@ class LoraReceiver : public Component {
       std::vector<uint8_t> buffer(size);
 
       int readsize = LoRa.readBytes(buffer.data(), size);
+      unsigned char* bytes = buffer.data();
+
       if(size!=readsize) {
-        Serial.println("[LORA] Received packet size mismatch");
+        ESP_LOGD(TAG, "Received packet size mismatch");
         return;
       }
 
       packetCounter++;
       lastPacketReceived=millis();
-      //print_buffer(buffer.data(), size);
 
-      Serial.printf("[LORA] Received packet %d of %d bytes with RSSI %d\n", packetCounter, size, LoRa.packetRssi());
+      id(last_packet_received).publish_state(byte_2_str(bytes,readsize));
+      //id(mqtt_client).publish("lora/receivedpacket", byte_2_str(bytes,readsize));
 
-      auto packet = dzb::Packet::deconstruct(buffer.data(), size);
+      last_packet_rssi->publish_state(LoRa.packetRssi());
+      packet_counter->publish_state(packetCounter);
+
+      ESP_LOGD(TAG, "Received packet %d of %d bytes with RSSI %d\n", packetCounter, size, LoRa.packetRssi());
+
+      auto packet = dzb::Packet::deconstruct(bytes, size);
       parse_packet(packet);
     }
 
   public:
-    sensor::Sensor *device_J7_batt_voltage = new sensor::Sensor();
-    sensor::Sensor *device_J7_batt_percent = new sensor::Sensor();
-    sensor::Sensor *device_J7_batt_islow = new sensor::Sensor();
-    sensor::Sensor *device_J7_pir_state = new sensor::Sensor();
-    sensor::Sensor *device_J7_alarm_isactive = new sensor::Sensor();
+    Sensor *last_packet_rssi = new Sensor();
+    Sensor *packet_counter = new sensor::Sensor();
 
-    sensor::Sensor *device_PT_batt_voltage = new sensor::Sensor();
-    sensor::Sensor *device_PT_batt_percent = new sensor::Sensor();
-    sensor::Sensor *device_PT_batt_islow = new sensor::Sensor();
-    sensor::Sensor *device_PT_water_level = new sensor::Sensor();
+    Sensor *device_J7_batt_voltage = new sensor::Sensor();
+    Sensor *device_J7_batt_percent = new sensor::Sensor();
+    Sensor *device_J7_batt_islow = new sensor::Sensor();
+    Sensor *device_J7_pir_state = new sensor::Sensor();
+    Sensor *device_J7_alarm_isactive = new sensor::Sensor();
+
+    Sensor *device_PT_batt_voltage = new sensor::Sensor();
+    Sensor *device_PT_batt_percent = new sensor::Sensor();
+    Sensor *device_PT_batt_islow = new sensor::Sensor();
+    Sensor *device_PT_water_level = new sensor::Sensor();
 
     LoraReceiver() : Component() {
     }
@@ -229,17 +256,8 @@ class LoraReceiver : public Component {
 
     void loop() override {
 
-      //display buffer update
-
       //TODO: better way to get wifi connection status from esphome?
       display.drawMainFrame(&state, packetCounter, WiFi.status()==WL_CONNECTED, lastPacketReceived);
-
-      //send 'gateway is alive' every minute
-      //if(((millis()-lastGatewayAliveReport)>(60*1000)) || (lastGatewayAliveReport==0)) {
-      //  //TODO: setstate with timestamp to see current status of the gateway sensor
-      //  lastGatewayAliveReport=millis();
-      //  return;
-      //}
 
       //refresh screen @24 fps
       if((millis()-lastScreenUpdate)>(1000/24)){

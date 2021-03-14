675e1920e0830488ca66628028138c68eea05853
DrZoid <docteurzoidberg@users.noreply.github.com>
2021-03-14 15:39:53 +0100

    DisplayFireEffect: before processing sketch

--- custom_components/zilloscope/display_effect.h
+++ custom_components/zilloscope/display_effect.h
@@ -2,6 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/display/display_buffer.h"
+#include <math.h>
 
 namespace esphome {
 namespace zilloscope {
@@ -56,17 +57,54 @@ namespace zilloscope {
 
   class DisplayFireEffect : public DisplayEffect {
     public:
-      explicit DisplayFireEffect(const std::string &name) : DisplayEffect(name) {}
-
       uint8_t counter = 0;
+      float cur[35][25];
+      float prev[35][25];
+      float v = 0.5f;
+      float a = 0.01f;
 
-      void apply(display::DisplayBuffer &it) override {
-        //TODO
-        if(counter%2==0) {
-          it.draw_pixel_at(0,0,Color(0xffffff));
+      explicit DisplayFireEffect(const std::string &name) : DisplayEffect(name) {
+        //todo: fill arrays?
+      }
+      uint32_t heat_color(float value) {
+        int r = (int) min(((int)(value * 255) * 3), 255);
+        int g = (int) max(0, min((int)((value - 0.33) * 255 * 3), 255));
+        int b = (int) max(0, min((int)((value - 0.66) * 255 * 3), 255));
+        return (r << 16) + (g << 8) + b; // là t'as p't'être une fonction toute faite qui te donne une couleur pour le triplet r, g, b correspondant a utiliser plutôt que ça
+        //return r + (r << 8) + (r << 16);
+      }
+      void start() override {
+         for(int a=0;a<35;a++) {
+          for(int b=0;b<25;b++) {
+            prev[a][b]=cur[a][b]=0;
+          }
         }
-        else {
-          it.draw_pixel_at(1,1,Color(0xffffff));
+      }
+      // code exécuté a chaque frame
+      void apply(display::DisplayBuffer &it) override {
+
+        for(int x = 0 ; x < it.get_width() ; x++)
+          for(int y = 0 ; y < it.get_height() ; y++) {
+            //=> ici, donc, les 0.1, 0.1, 0.1 et 0.7 servent a orienter la flamme
+            //la somme des 4 valeurs doit faire 1
+            //float tmp = prev[max(x-1, 0)][y] * 0.1 + prev[x][max(y-1, 0)] * 0.1 + prev[min(x+1, it.get_width() - 1)][y] * 0.1 + prev[x][min(y+1, it.get_height() - 1)] * 0.7;
+            float tmp = prev[max(x-1, 0)][y] * 0.01 + prev[x][max(y-1, 0)] * 0.01 + prev[min(x+1, it.get_width() - 1)][y] * 0.01 + prev[x][min(y+1, it.get_height() - 1)] * 0.97;
+
+            //cur[x][y] = tmp * 0.1 + prev[x][y] * 0.89;
+            cur[x][y] = tmp * v + prev[x][y] * (1 - v - a);
+            it.draw_pixel_at(x, y, Color(heat_color(cur[x][y])));
+          }
+
+        //cur[0][0]=1;
+        for(int x = 0 ; x < it.get_width() ; x ++)
+          if((esp_random() & 31) == 0)
+            cur[x][it.get_height() - 1] = 1;
+
+        //prev = cur;
+        for(int a=0;a<it.get_width();a++) {
+          for(int b=0;b<it.get_height();b++) {
+            prev[a][b]=cur[a][b];
+          }
         }
         counter++;
       }

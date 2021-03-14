808d59c2f132f8322c7d1a51074bb5f20f2905cf
DrZoid <docteurzoidberg@users.noreply.github.com>
2021-03-14 16:13:22 +0100

    DisplayFireEffect: after processing sketch

--- custom_components/zilloscope/display_effect.h
+++ custom_components/zilloscope/display_effect.h
@@ -57,15 +57,60 @@ namespace zilloscope {
 
   class DisplayFireEffect : public DisplayEffect {
     public:
-      uint8_t counter = 0;
-      float cur[35][25];
-      float prev[35][25];
-      float v = 0.5f;
-      float a = 0.01f;
+      float max_flame_height = 1;
+      float max_heat_spot = 1;
+      float min_x_attenuation = 0.2;
+      float init_flame_height = 0;
+      float init_heat_spots = 0;
+      float init_x_attenuation = 2;
+      float speed = 10;
+      float starting_speed = 1;
 
       explicit DisplayFireEffect(const std::string &name) : DisplayEffect(name) {
-        //todo: fill arrays?
+
       }
+/*
+      float rnd(int x, int y) {
+        int X = x ^ 6428;
+        int Y = y ^ 6356;
+        return ((
+          X * 7151
+          + Y * 1357)
+          ^ 35135) / 137.0f % 1.0f;
+      }
+*/
+      float rnd(int x, int y) {
+        return esp_random()/UINT32_MAX;
+      }
+
+      float noise(int w, int h, int X, int Y, int T, float flame_height, float heat_spots, float x_attenuation) {
+
+        int x = X;
+        float attenuation = (h - Y) / flame_height / h + pow(1 - (X + 1) * (w - X) * 4.0f / (w + 2) / (w + 2), 1 / x_attenuation);
+        float n = 0;
+        float sum_coeff = 0;
+
+        for(int i = 8 ; i > 0 ; i >>= 1) {
+          int y = Y + T * 8 / i;
+
+          float rnd_00 = pow(rnd(x / i, y / i), 1 / heat_spots / (attenuation + 1));
+          float rnd_01 = pow(rnd(x / i, y / i + 1), 1 / heat_spots / (attenuation + 1));
+          float rnd_10 = pow(rnd(x / i + 1, y / i), 1 / heat_spots / (attenuation + 1));
+          float rnd_11 = pow(rnd(x / i + 1, y / i + 1), 1 / heat_spots / (attenuation + 1));
+
+          float dx = x * 1.0f / i - x / i;
+          float dy = y * 1.0f / i - y / i;
+
+          float coeff = i;
+
+          n += ((rnd_00 * (1 - dx) + rnd_10 * dx) * (1 - dy)
+            + (rnd_01 * (1 - dx) + rnd_11 * dx) * dy) * coeff;
+          sum_coeff += coeff;
+        }
+
+        return max(0.0f, n / sum_coeff - attenuation);
+      }
+
       uint32_t heat_color(float value) {
         int r = (int) min(((int)(value * 255) * 3), 255);
         int g = (int) max(0, min((int)((value - 0.33) * 255 * 3), 255));
@@ -73,50 +118,26 @@ namespace zilloscope {
         return (r << 16) + (g << 8) + b; // là t'as p't'être une fonction toute faite qui te donne une couleur pour le triplet r, g, b correspondant a utiliser plutôt que ça
         //return r + (r << 8) + (r << 16);
       }
+
       void start() override {
-         for(int a=0;a<35;a++) {
-          for(int b=0;b<25;b++) {
-            prev[a][b]=cur[a][b]=0;
-          }
-        }
+
       }
       // code exécuté a chaque frame
       void apply(display::DisplayBuffer &it) override {
-
-        for(int x = 0 ; x < it.get_width() ; x++)
-          for(int y = 0 ; y < it.get_height() ; y++) {
-            //=> ici, donc, les 0.1, 0.1, 0.1 et 0.7 servent a orienter la flamme
-            //la somme des 4 valeurs doit faire 1
-            //float tmp = prev[max(x-1, 0)][y] * 0.1 + prev[x][max(y-1, 0)] * 0.1 + prev[min(x+1, it.get_width() - 1)][y] * 0.1 + prev[x][min(y+1, it.get_height() - 1)] * 0.7;
-            float tmp = prev[max(x-1, 0)][y] * 0.01 + prev[x][max(y-1, 0)] * 0.01 + prev[min(x+1, it.get_width() - 1)][y] * 0.01 + prev[x][min(y+1, it.get_height() - 1)] * 0.97;
-
-            //cur[x][y] = tmp * 0.1 + prev[x][y] * 0.89;
-            cur[x][y] = tmp * v + prev[x][y] * (1 - v - a);
-            it.draw_pixel_at(x, y, Color(heat_color(cur[x][y])));
-          }
-
-        //cur[0][0]=1;
-        for(int x = 0 ; x < it.get_width() ; x ++)
-          if((esp_random() & 31) == 0)
-            cur[x][it.get_height() - 1] = 1;
-
-        //prev = cur;
-        for(int a=0;a<it.get_width();a++) {
-          for(int b=0;b<it.get_height();b++) {
-            prev[a][b]=cur[a][b];
+        float flame_height = min(init_flame_height + starting_speed * millis() / 1000.0f, max_flame_height);
+        float heat_spots = min(init_heat_spots + starting_speed * millis() / 1000.0f, max_flame_height);
+        float x_attenuation = max(init_x_attenuation - starting_speed * millis() / 1000.0f, min_x_attenuation);
+        for(int x = 0 ; x < it.get_width(); x ++) {
+          for(int y = 0 ; y < it.get_height(); y ++) {
+            uint32_t heat = heat_color(noise(it.get_width(), it.get_height(), x, y, (int) (millis() * speed / 1000), flame_height, heat_spots, x_attenuation));
+            //fill(red(heat), green(heat), blue(heat));
+            //rect(x * 16, y * 16, 16, 16);
+            it.draw_pixel_at(x,y,Color(heat));
           }
         }
-        counter++;
       }
 
-      void set_speed(uint32_t speed) { this->speed_ = speed; }
-      void set_width(uint16_t width) { this->width_ = width; }
-      void set_height(uint16_t height) { this->height_ = height; }
-
     protected:
-      uint32_t speed_{10};
-      uint16_t width_{8};
-      uint16_t height_{8};
   };
 
   class DisplayRainbowEffect : public DisplayEffect {
